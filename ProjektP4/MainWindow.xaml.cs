4787cd6632da518365ad71db0eff9c3e8ce01802
NeverLyne <michal.jan.jarosz@gmail.com>
2023-04-22 12:12:59 +0200

    Dodaj pliki projektów.

---
+++ ProjektP4/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektP4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Combo1.ItemsSource = new string[] {"Brzeg", "Opole", "Wrocław" };
+            Combo2.ItemsSource = new string[] { "Brzeg", "Opole", "Wrocław" };
+        }
+
+        private void Sprawdz_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 Noweokno = new Window1();
+            Noweokno.ShowDialog();
+        }
+
+       
+    }
+}
---
+++ ProjektP4/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektP4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            TextBlock1.Inlines.Add(new Run("Stacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja"));
+            TextBlock2.Inlines.Add(new Run("Stacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja"));
+            TextBlock3.Inlines.Add(new Run("Godzina\nGodzina\nGodzina\nGodzina\nGodzina\nGodzina\nGodzina\nGodzina\n"));
+            TextBlock4.Inlines.Add(new Run("Data\nData\nData\nData\nData\nData\nData\nData\nData\nData\n"));
+
+
+        }
+
+        private void Wroc_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
