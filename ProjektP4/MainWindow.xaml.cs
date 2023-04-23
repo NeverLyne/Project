121411db8db1a42cb385affedf0158a11a9342ef
Michal Jarosz <262236@student.pwr.edu.pl>
2023-04-23 19:05:00 +0200

    Numeric updown

--- ProjektP4/MainWindow.xaml.cs
+++ ProjektP4/MainWindow.xaml.cs
@@ -26,6 +26,103 @@ namespace ProjektP4
             Combo1.ItemsSource = new string[] {"Brzeg", "Opole", "Wrocław" };
             Combo2.ItemsSource = new string[] { "Brzeg", "Opole", "Wrocław" };
         }
+        private int _numValue = 0;
+        
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        public void NumberUpDown()
+        {
+            
+            InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+
+            if (NumValue <= 0)
+            {
+                txtNum.Text = "0";
+            }
+            if (NumValue > 23)
+            {
+                txtNum.Text = "23";
+            }
+        }
+
+        private int _numValue1 = 0;
+
+        public int NumValue1
+        {
+            get { return _numValue1; }
+            set
+            {
+                _numValue1 = value;
+                txtNum1.Text = value.ToString();
+            }
+        }
+
+        public void NumberUpDown1()
+        {
+
+            InitializeComponent();
+            txtNum1.Text = _numValue1.ToString();
+        }
+
+        private void cmdUp_Click1(object sender, RoutedEventArgs e)
+        {
+            NumValue1++;
+        }
+
+        private void cmdDown_Click1(object sender, RoutedEventArgs e)
+        {
+            NumValue1--;
+        }
+
+        private void txtNum_TextChanged1(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum1 == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum1.Text, out _numValue1))
+                txtNum1.Text = _numValue1.ToString();
+
+            if(NumValue1 <= 0)
+            {
+                txtNum1.Text = "00";
+            }
+            if (NumValue1 > 59)
+            {
+                txtNum1.Text = "59";
+            }
+        }
 
         private void Sprawdz_Click(object sender, RoutedEventArgs e)
         {
