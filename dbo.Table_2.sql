814310c1d77cf57b14981f904a3927459ab00ce3
NeverLyne <michal.jan.jarosz@gmail.com>
2023-05-15 12:29:32 +0200

    baza

---
+++ dbo.Table_2.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE schedules (
+  id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  train_id INT NOT NULL,
+  origin_station_id INT NOT NULL,
+  destination_station_id INT NOT NULL,
+  departure_time DATETIME2 NOT NULL,
+  arrival_time DATETIME2 NOT NULL,
+  FOREIGN KEY (train_id) REFERENCES trains(id),
+  FOREIGN KEY (origin_station_id) REFERENCES stations(id),
+  FOREIGN KEY (destination_station_id) REFERENCES stations(id)
+);
