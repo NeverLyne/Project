fa0114818fc8e1be003b8f149ca923309e3f8f51
Michal Jarosz <262236@student.pwr.edu.pl>
2023-05-21 16:34:41 +0200

    Baza danych zaczyna dzialac

--- ProjektP4/Window1.xaml.cs
+++ ProjektP4/Window1.xaml.cs
@@ -22,12 +22,32 @@ namespace ProjektP4
         public Window1()
         {
             InitializeComponent();
-            TextBlock1.Inlines.Add(new Run("Stacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja"));
-            TextBlock2.Inlines.Add(new Run("Stacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja\nStacja"));
-            TextBlock3.Inlines.Add(new Run("Godzina\nGodzina\nGodzina\nGodzina\nGodzina\nGodzina\nGodzina\nGodzina\n"));
-            TextBlock4.Inlines.Add(new Run("Data\nData\nData\nData\nData\nData\nData\nData\nData\nData\n"));
+            Database1Entities db = new Database1Entities();
+            var polaczenia = from s in db.schedules 
+                             join o in db.stations on s.origin_station_id equals o.id 
+                             join d in db.stations on s.destination_station_id equals d.id
+                             join t in db.trains on s.train_id equals t.id
 
+                             where o.name.Equals("opole glowne")
 
+                             select new
+                             {
+                                 StacjaPoczatkowa = o.name,
+                                 StacjaKoncowa =  d.name,
+                                 CzasOdjazdu = s.departure_time,
+                                 CzasPrzyjazdu = s.arrival_time,
+                                 NazwaPociagu = t.name
+                             };
+            foreach (var item in polaczenia)
+            {
+                Console.WriteLine(item.StacjaKoncowa);
+                Console.WriteLine(item.StacjaPoczatkowa);
+                Console.WriteLine(item.CzasOdjazdu);
+                Console.WriteLine(item.CzasPrzyjazdu);
+                Console.WriteLine(item.NazwaPociagu);
+
+            }
+            this.polaczenia.ItemsSource = polaczenia.ToList();
         }
 
         private void Wroc_Click(object sender, RoutedEventArgs e)
